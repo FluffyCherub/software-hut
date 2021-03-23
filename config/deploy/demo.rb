b6c856eda1f65ce051fcaa5ab12538a9de6c1e71
Ling Lai <llai2@sheffield.ac.uk>
2021-03-23 20:35:29 +0000

    fixing the ruby2.6.6 error

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -4,6 +4,12 @@ set :user,        'demo.team22'
 set :deploy_to,   -> { "/srv/services/#{fetch(:user)}" }
 set :log_level,   :debug
 
+## added below line due to an error: 'ruby stdout: Ruby ruby-2.6.6 is not installed.''
+## https://vle.shef.ac.uk/ultra/courses/_90050_1/cl/outline?legacyUrl=%252Fwebapps%252Fblackboard%252Fexecute%252Fannouncement%3Fmethod%3Dsearch%26context%3Dmybb%26course_id%3D_90050_1%26individualAnnouncementId%3D_183481_1
+set :rvm_ruby_version, 'ruby-2.6.2'
+
+
+
 ## Server configuration
 server fetch(:server), user: fetch(:user), roles: %w{web app db}
 
