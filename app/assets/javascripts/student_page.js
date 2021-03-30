56cbffd3cee196cf58a6a8f28bb6a845418749e7
Anton Minkov <aminkov1@sheffield.ac.uk>
2021-03-30 03:03:50 +0100

    responsive text added + dynamic updating

--- app/assets/javascripts/student_page.js
+++ app/assets/javascripts/student_page.js
@@ -13,3 +13,15 @@ function getSelectedModule() {
   document.getElementById("module_name").innerHTML = strUser;
 
 }
+function sameHeightGroup() {
+  var feedbackHeight = document.getElementById('module_feedback').clientHeight;
+  var strHeight = feedbackHeight.toString();
+  var px = "px";
+  var finalHeight = strHeight.concat(px);
+  var groupDiv = document.getElementById('module_group');
+  groupDiv.style.height = finalHeight;
+}
+
+$(function resizeGroup(){
+  setInterval(sameHeightGroup, 200);
+  });
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_152733) do
+ActiveRecord::Schema.define(version: 2021_03_29_213600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,14 +41,6 @@ ActiveRecord::Schema.define(version: 2021_03_29_152733) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "list_modules_users", id: false, force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "list_module_id", null: false
-    t.string "privilege"
-    t.index ["list_module_id", "user_id"], name: "index_list_modules_users_on_list_module_id_and_user_id"
-    t.index ["user_id", "list_module_id"], name: "index_list_modules_users_on_user_id_and_list_module_id"
-  end
-
   create_table "sessions", force: :cascade do |t|
     t.string "session_id", null: false
     t.text "data"
@@ -56,6 +48,16 @@ ActiveRecord::Schema.define(version: 2021_03_29_152733) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
     t.index ["updated_at"], name: "index_sessions_on_updated_at"
+  end
+
+  create_table "user_list_modules", force: :cascade do |t|
+    t.bigint "list_module_id", null: false
+    t.bigint "user_id", null: false
+    t.string "privilege"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_module_id"], name: "index_user_list_modules_on_list_module_id"
+    t.index ["user_id"], name: "index_user_list_modules_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -81,8 +83,11 @@ ActiveRecord::Schema.define(version: 2021_03_29_152733) do
     t.string "dn"
     t.string "sn"
     t.string "givenname"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["username"], name: "index_users_on_username"
   end
 
+  add_foreign_key "user_list_modules", "list_modules"
+  add_foreign_key "user_list_modules", "users"
 end
