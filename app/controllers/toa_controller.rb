ab4a3bc51e524cee76c377b5ee61f676d5762200
Anton Minkov <aminkov1@sheffield.ac.uk>
2021-04-01 16:51:26 +0100

    updating toa form

--- app/controllers/toa_controller.rb
+++ app/controllers/toa_controller.rb
@@ -1,4 +1,14 @@
 class ToaController < ApplicationController
+
+  helper_method :update_toa
+
   def toa_doc
   end
+
+  def update_toa
+    if params["update_form"] != nil
+      puts params["update_form"]["member2signature"]
+    end
+  end
+
 end
