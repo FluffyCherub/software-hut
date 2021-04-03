c97f19feb983cc8023e4aee58b6b7445892db0ad
Dominik <dlaszczyk1@sheffield.ac.uk>
2021-04-04 00:38:39 +0100

    block sending actual emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,8 +4,8 @@ Rails.application.configure do
   config.reload_classes_only_on_change = false
 
   # Preview email in the browser instead of sending it
-  # config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
