043e64e91badb06f624a7aae26ed02b0ecaff5ba
Dominik <dlaszczyk1@sheffield.ac.uk>
2021-04-05 00:08:00 +0100

    javascript passing username

--- app/assets/javascripts/admin_page.js
+++ app/assets/javascripts/admin_page.js
@@ -1,7 +1,10 @@
 function userFunction(username) {
-  alert(username);
+  
   document.getElementById('form_username').value = username;
-  document.getElementById("privilege_form").submit();
+  var test_var =document.getElementById('form_username').value
+  alert(test_var)
+  var x = document.getElementsByClassName('privilege_form');
+  x[0].submit();
   
 }
 
