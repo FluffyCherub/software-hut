735cfba3403c9788c761ab1fe5be80f0c9411ed1
Anton Minkov <aminkov1@sheffield.ac.uk>
2021-04-19 05:24:40 +0100

    Adding multiple meeting times added/group update

--- app/assets/javascripts/adding_topics.js
+++ app/assets/javascripts/adding_topics.js
@@ -27,15 +27,446 @@ function removeTopic() {
   }
 }
 
-function topicButtonChange() {
-  var button_value = document.getElementById("topic_toggle").value;  
+function topicButtonChange(students_in_module) {
+  var button_value = document.getElementById("topic_toggle").value;
+  var normal_value = document.getElementById("normal_toggle").value;  
 
   if (button_value == "disabled") {
-    document.querySelector('#topic_toggle').innerHTML = 'Disable Topics';
-    document.getElementById("topic_toggle").value = "enabled"  
+
+    if (normal_value == "disabled") {
+      document.querySelector('#topic_toggle').innerHTML = 'Disable Topics';
+      document.getElementById("topic_toggle").value = "enabled"
+      
+      setInterval(function() {
+              
+        var number_of_topics = document.getElementById("total_chq").value;
+
+        var studentNumberTotal = students_in_module;
+        var studentNumberCurrent = 0;
+        var totalGroups = 0;
+
+        for (var i = 0; i < number_of_topics; i++) {
+          var students_per_group;
+          var amout_of_groups;
+
+          var id_number = i + 1;
+          var id_size = 'size_' + id_number;
+          var id_amount = 'amount_' + id_number;
+
+          if (document.getElementById(id_size).value) {
+            students_per_group = document.getElementById(id_size).value;
+          }
+          else {
+            students_per_group = 0;
+          };
+
+          if (document.getElementById(id_amount).value) {
+            amout_of_groups = document.getElementById(id_amount).value;
+          }
+          else {
+            amout_of_groups = 0;
+          };
+
+          var students_for_topic = students_per_group*amout_of_groups;
+          totalGroups += amout_of_groups
+          studentNumberCurrent += students_for_topic;
+        }
+
+        var empty_spots = studentNumberCurrent - studentNumberTotal;
+
+        if (studentNumberTotal == studentNumberCurrent) {
+          document.getElementById("student_tracker").innerHTML = "All students are in a team!";
+          document.getElementById("student_tracker").style.color = "green";
+          document.getElementById("spot_tracker").innerHTML = "(" + empty_spots + ") empty slots!";
+          document.getElementById("spot_tracker").style.color = "green"
+        }
+        else if (studentNumberTotal > studentNumberCurrent) {
+          var groupless_students = studentNumberTotal - studentNumberCurrent;
+
+          document.getElementById("student_tracker").innerHTML = "(" + groupless_students + ") students are not in a team!";
+          document.getElementById("student_tracker").style.color = "red";
+          document.getElementById("spot_tracker").innerHTML = "(0) empty slots!";
+          document.getElementById("spot_tracker").style.color = "red"
+        }
+        else {
+          empty_spots = studentNumberCurrent - studentNumberTotal;
+
+          if (empty_spots <= number_of_topics) {
+            document.getElementById("student_tracker").innerHTML = "All students have teams";
+            document.getElementById("student_tracker").style.color = "green";
+            document.getElementById("spot_tracker").innerHTML = "(" + empty_spots + ") empty slots!";
+            document.getElementById("spot_tracker").style.color = "green"
+          }
+          else {
+            document.getElementById("student_tracker").innerHTML = "All students have teams";
+            document.getElementById("student_tracker").style.color = "red";
+            document.getElementById("spot_tracker").innerHTML = "(" + empty_spots + ") empty slots!";
+            document.getElementById("spot_tracker").style.color = "red"
+          } ;
+        };
+      }, 100);
+    }
+    else {
+      document.getElementById("normal_toggle").value = "disabled"
+      var normal_div = document.getElementById("normalCollapse");
+      normal_div.classList.remove("show");
+      document.getElementById("normal_toggle").setAttribute("aria-expanded", "false");
+
+      document.querySelector('#topic_toggle').innerHTML = 'Disable Topics';
+      document.getElementById("topic_toggle").value = "enabled"
+      
+      setInterval(function() {
+              
+        var number_of_topics = document.getElementById("total_chq").value;
+
+        var studentNumberTotal = students_in_module;
+        var studentNumberCurrent = 0;
+        var totalGroups = 0;
+
+        for (var i = 0; i < number_of_topics; i++) {
+          var students_per_group;
+          var amout_of_groups;
+
+          var id_number = i + 1;
+          var id_size = 'size_' + id_number;
+          var id_amount = 'amount_' + id_number;
+
+          if (document.getElementById(id_size).value) {
+            students_per_group = document.getElementById(id_size).value;
+          }
+          else {
+            students_per_group = 0;
+          };
+
+          if (document.getElementById(id_amount).value) {
+            amout_of_groups = document.getElementById(id_amount).value;
+          }
+          else {
+            amout_of_groups = 0;
+          };
+
+          var students_for_topic = students_per_group*amout_of_groups;
+          totalGroups += amout_of_groups
+          studentNumberCurrent += students_for_topic;
+        }
+
+        var empty_spots = studentNumberCurrent - studentNumberTotal;
+
+        if (studentNumberTotal == studentNumberCurrent) {
+          document.getElementById("student_tracker").innerHTML = "All students are in a team!";
+          document.getElementById("student_tracker").style.color = "green";
+          document.getElementById("spot_tracker").innerHTML = "(" + empty_spots + ") empty slots!";
+          document.getElementById("spot_tracker").style.color = "green"
+        }
+        else if (studentNumberTotal > studentNumberCurrent) {
+          var groupless_students = studentNumberTotal - studentNumberCurrent;
+
+          document.getElementById("student_tracker").innerHTML = "(" + groupless_students + ") students are not in a team!";
+          document.getElementById("student_tracker").style.color = "red";
+          document.getElementById("spot_tracker").innerHTML = "(0) empty slots!";
+          document.getElementById("spot_tracker").style.color = "red"
+        }
+        else {
+          empty_spots = studentNumberCurrent - studentNumberTotal;
+
+          if (empty_spots <= number_of_topics) {
+            document.getElementById("student_tracker").innerHTML = "All students have teams";
+            document.getElementById("student_tracker").style.color = "green";
+            document.getElementById("spot_tracker").innerHTML = "(" + empty_spots + ") empty slots!";
+            document.getElementById("spot_tracker").style.color = "green"
+          }
+          else {
+            document.getElementById("student_tracker").innerHTML = "All students have teams";
+            document.getElementById("student_tracker").style.color = "red";
+            document.getElementById("spot_tracker").innerHTML = "(" + empty_spots + ") empty slots!";
+            document.getElementById("spot_tracker").style.color = "red"
+          } ;
+        };
+      }, 100);
+    }
+
   }
   else {
     document.querySelector('#topic_toggle').innerHTML = 'Enable Topics';
     document.getElementById("topic_toggle").value = "disabled"  
+  }
+}
+
+function normalButtonChange(students_in_module) {
+  var button_value = document.getElementById("normal_toggle").value;
+  var toggle_value = document.getElementById("topic_toggle").value;  
+
+  if (button_value == "disabled") {
+
+    if (toggle_value == "disabled") {
+      document.getElementById("normal_toggle").value = "enabled";
+
+      setInterval(function() {
+                   
+        var studentNumberTotal = students_in_module;
+        var studentNumberCurrent = 0;
+
+        var students_per_group;
+        var amount_of_groups;
+
+        if (document.getElementById("normal_size").value) {
+          students_per_group = document.getElementById("normal_size").value;
+        }
+        else {
+          students_per_group = 0;
+        };
+
+        if (document.getElementById("normal_amount").value) {
+          amount_of_groups = document.getElementById("normal_amount").value;
+        }
+        else {
+          amount_of_groups = 0;
+        };
+
+        studentNumberCurrent = students_per_group*amount_of_groups
+
+        var empty_spots = studentNumberCurrent - studentNumberTotal;
+
+        if (studentNumberTotal == studentNumberCurrent) {
+          document.getElementById("student_tracker_normal").innerHTML = "All students are in a team!";
+          document.getElementById("student_tracker_normal").style.color = "green";
+          document.getElementById("spot_tracker_normal").innerHTML = "(" + empty_spots + ") empty slots!";
+          document.getElementById("spot_tracker_normal").style.color = "green"
+        }
+        else if (studentNumberTotal > studentNumberCurrent) {
+          var groupless_students = studentNumberTotal - studentNumberCurrent;
+
+          document.getElementById("student_tracker_normal").innerHTML = "(" + groupless_students + ") students are not in a team!";
+          document.getElementById("student_tracker_normal").style.color = "red";
+          document.getElementById("spot_tracker_normal").innerHTML = "(0) empty slots!";
+          document.getElementById("spot_tracker_normal").style.color = "red"
+        }
+        else {
+          empty_spots = studentNumberCurrent - studentNumberTotal;
+
+          if (empty_spots < amount_of_groups) {
+            document.getElementById("student_tracker_normal").innerHTML = "All students have teams";
+            document.getElementById("student_tracker_normal").style.color = "green";
+            document.getElementById("spot_tracker_normal").innerHTML = "(" + empty_spots + ") empty slots!";
+            document.getElementById("spot_tracker_normal").style.color = "green"
+          }
+          else {
+            document.getElementById("student_tracker_normal").innerHTML = "All students have teams";
+            document.getElementById("student_tracker_normal").style.color = "red";
+            document.getElementById("spot_tracker_normal").innerHTML = "(" + empty_spots + ") empty slots!";
+            document.getElementById("spot_tracker_normal").style.color = "red"
+          } ;
+        }
+
+      }, 100);
+    }
+    else {
+      document.querySelector('#topic_toggle').innerHTML = 'Enable Topics';
+      document.getElementById("topic_toggle").value = "disabled"
+      var topic_div = document.getElementById("topicCollapse");
+      topic_div.classList.remove("show");
+      document.getElementById("topic_toggle").setAttribute("aria-expanded", "false");
+
+      document.getElementById("normal_toggle").value = "enabled";
+
+      setInterval(function() {
+                   
+        var studentNumberTotal = students_in_module;
+        var studentNumberCurrent = 0;
+
+        var students_per_group;
+        var amount_of_groups;
+
+        if (document.getElementById("normal_size").value) {
+          students_per_group = document.getElementById("normal_size").value;
+        }
+        else {
+          students_per_group = 0;
+        };
+
+        if (document.getElementById("normal_amount").value) {
+          amount_of_groups = document.getElementById("normal_amount").value;
+        }
+        else {
+          amount_of_groups = 0;
+        };
+
+        studentNumberCurrent = students_per_group*amount_of_groups
+
+        var empty_spots = studentNumberCurrent - studentNumberTotal;
+
+        if (studentNumberTotal == studentNumberCurrent) {
+          document.getElementById("student_tracker_normal").innerHTML = "All students are in a team!";
+          document.getElementById("student_tracker_normal").style.color = "green";
+          document.getElementById("spot_tracker_normal").innerHTML = "(" + empty_spots + ") empty slots!";
+          document.getElementById("spot_tracker_normal").style.color = "green"
+        }
+        else if (studentNumberTotal > studentNumberCurrent) {
+          var groupless_students = studentNumberTotal - studentNumberCurrent;
+
+          document.getElementById("student_tracker_normal").innerHTML = "(" + groupless_students + ") students are not in a team!";
+          document.getElementById("student_tracker_normal").style.color = "red";
+          document.getElementById("spot_tracker_normal").innerHTML = "(0) empty slots!";
+          document.getElementById("spot_tracker_normal").style.color = "red"
+        }
+        else {
+          empty_spots = studentNumberCurrent - studentNumberTotal;
+
+          if (empty_spots < amount_of_groups) {
+            document.getElementById("student_tracker_normal").innerHTML = "All students have teams";
+            document.getElementById("student_tracker_normal").style.color = "green";
+            document.getElementById("spot_tracker_normal").innerHTML = "(" + empty_spots + ") empty slots!";
+            document.getElementById("spot_tracker_normal").style.color = "green"
+          }
+          else {
+            document.getElementById("student_tracker_normal").innerHTML = "All students have teams";
+            document.getElementById("student_tracker_normal").style.color = "red";
+            document.getElementById("spot_tracker_normal").innerHTML = "(" + empty_spots + ") empty slots!";
+            document.getElementById("spot_tracker_normal").style.color = "red"
+          } ;
+        }
+
+      }, 100);
+    }     
+
+  }
+  else {
+    document.getElementById("normal_toggle").value = "disabled"  
+  }
+}
+
+function tempDisableButton() {
+  setTimeout(function() {
+    document.getElementById('normal_toggle').disabled = true;
+    document.getElementById('topic_toggle').disabled = true;
+  }, 50);
+  setTimeout(function() {
+    document.getElementById('normal_toggle').disabled = false;
+    document.getElementById('topic_toggle').disabled = false;
+  }, 400);
+}
+
+function addPeriod() {
+  var new_chq_no = parseInt($('#total_chq_period').val()) + 1;
+
+  //Creating new elements
+  //Divs
+  var input_div_1 = "<div class='container-fluid text_black bg_none margin_bottom_small no_padding float_left' id='div_" + new_chq_no + "'> ";
+  var input_div_2 = "<div class='container-fluid text_black bg_none special_size_1 margin_bottom_small no_padding float_left' id='div_2_" + new_chq_no + "'> ";
+  var input_div_3 = "<div class='container-fluid text_black bg_none margin_bottom_small no_padding' id='div_2_3" + new_chq_no + "'> ";
+  var input_p_1 = "<p class='margin_none' id='p_name_" + new_chq_no + "'> ";
+  var input_strong_1 = "<strong id='time_name_" + new_chq_no + "'> ";
+
+  //Start Date
+  var picker_div_start = "<div class='container-fluid text_black bg_none no_padding margin_bottom_small' id='picker_div_start_" + new_chq_no + "'> ";
+  var label_start = "<label id='start_time_label_" + new_chq_no + "'> ";
+  var date_start = "<input class='float_right' id='start_time_" + new_chq_no + "'> ";
+
+  //End Date
+  var picker_div_end = "<div class='container-fluid text_black bg_none no_padding margin_bottom_small' id='picker_div_end_" + new_chq_no + "'> ";
+  var label_end = "<label id='end_time_label_" + new_chq_no + "'> ";
+  var date_end = "<input class='float_right' id='end_time_" + new_chq_no + "'> ";
+
+  //Separator
+  var line_after = "<hr class='special_line margin_none' id='line_after_" + new_chq_no + "'> ";
+
+  //Div ID's
+  var input_div_1_id = '#div_' + new_chq_no.toString();
+  var input_div_2_id = '#div_2_' + new_chq_no.toString();
+  var input_div_3_id = '#div_2_3' + new_chq_no.toString();
+  var input_p_1_id = '#p_name_' + new_chq_no.toString();
+  var input_strong_1_id = '#time_name_' + new_chq_no.toString();
+  
+  //Start Date ID's
+  var picker_div_start_id = '#picker_div_start_' + new_chq_no.toString();
+  var label_start_id = '#start_time_label_' + new_chq_no.toString();
+  var date_start_id = '#start_time_' + new_chq_no.toString();
+  
+  //End Date ID's
+  var picker_div_end_id = '#picker_div_end_' + new_chq_no.toString();
+  var label_end_id = '#end_time_label_' + new_chq_no.toString();
+  var date_end_id = '#end_time_' + new_chq_no.toString();
+  
+  //Separator ID's
+  var line_after_id = '#line_after_' + new_chq_no.toString();
+
+  //Appending the elements
+  $('#new_chq_period').append(input_div_1);
+  
+  //Main Div
+  $(input_div_1_id).append(input_div_2);
+  
+  //Divs
+  $(input_div_2_id).append(input_div_3);
+  $(input_div_3_id).append(input_p_1);
+  $(input_p_1_id).append(input_strong_1);
+  
+  //Start Date
+  $(input_div_2_id).append(picker_div_start);
+  $(picker_div_start_id).append(label_start);
+  $(picker_div_start_id).append(date_start);
+  
+  //End Date
+  $(input_div_2_id).append(picker_div_end);
+  $(picker_div_end_id).append(label_end);
+  $(picker_div_end_id).append(date_end);
+  
+  //Separator
+  $(input_div_2_id).append(line_after);
+
+  //Changing properties of created elements
+
+  //Name
+  document.getElementById('time_name_' + new_chq_no.toString()).innerHTML = "Meeting "  + new_chq_no.toString();
+
+  //Start Date
+  document.getElementById('start_time_label_' + new_chq_no.toString()).innerHTML = "Start Date: ";
+  document.getElementById('start_time_label_' + new_chq_no.toString()).htmlFor = date_start_id;
+  document.getElementById('start_time_' + new_chq_no.toString()).name = "start_time_"  + new_chq_no.toString();
+  document.getElementById('start_time_' + new_chq_no.toString()).type = "datetime-local";
+  document.getElementById('start_time_' + new_chq_no.toString()).value = "2018-06-12T19:30";
+
+  //End Date
+  document.getElementById('end_time_label_' + new_chq_no.toString()).innerHTML = "End Date: ";
+  document.getElementById('end_time_label_' + new_chq_no.toString()).htmlFor = date_end_id;
+  document.getElementById('end_time_' + new_chq_no.toString()).name = "end_time_"  + new_chq_no.toString();
+  document.getElementById('end_time_' + new_chq_no.toString()).type = "datetime-local";
+  document.getElementById('end_time_' + new_chq_no.toString()).value = "2018-06-12T19:30";
+
+  //Update counter value
+  $('#total_chq_period').val(new_chq_no);
+
+}
+
+function removePeriod() {
+  var last_chq_no = $('#total_chq_period').val();
+
+  if (last_chq_no > 1) {
+
+    //Removing elements
+    //Divs
+    $('#div_' + last_chq_no).remove();
+    $('#div_2_' + last_chq_no).remove();
+    $('#div_2_3' + last_chq_no).remove();
+    $('#p_name_' + last_chq_no).remove();
+
+    //Name
+    $('#time_name_' + last_chq_no).remove();
+
+    //Start Date
+    $('#picker_div_start_' + last_chq_no).remove();
+    $('#start_time_label_' + last_chq_no).remove();
+    $('#start_time_' + last_chq_no).remove();
+
+    //End Date
+    $('#picker_div_end_' + last_chq_no).remove();
+    $('#end_time_label_' + last_chq_no).remove();
+    $('#end_time_' + last_chq_no).remove();
+
+    //Separator
+    $('#line_after_' + last_chq_no).remove();
+
+    //Update counter value
+    $('#total_chq_period').val(last_chq_no - 1);
   }
 }
