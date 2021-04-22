041dcc684d6cc108f8fbca6650f02c776ec2f0f9
Dominik <dlaszczyk1@sheffield.ac.uk>
2021-04-22 04:26:20 +0100

    link to student groups join page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -56,5 +56,116 @@ class PagesController < ApplicationController
     end
   end
 
+  def student_groups_join
+    authorize! :manage, :admin_modules_groups
+
+    #getting a list of ta's and modules leaders for assigning problems
+    @ta_and_mod_lead = []
+    users_in_module = ListModule.users_in_module(params['module_id'])
+    for i in 0..(users_in_module.length-1) 
+      current_user_privilege = ListModule.privilege_for_module(users_in_module[i].username, params['module_id'])
+      
+      
+      current_user_names = users_in_module[i].givenname + " " + users_in_module[i].sn + " - " + users_in_module[i].username
+      if current_user_privilege.include?("teaching_assistant") || current_user_privilege.include?("module_leader")
+        @ta_and_mod_lead.append(current_user_names)
+      end
+    end
+
+    #assigning and solving problems
+    if params['problem_form'] != nil
+      problem_id = params['problem_form']['form_problem_id']
+      if params['assign_button'] == "Assign"
+        user_to_assign = params['problem_form']['assign_list'].split(" ")[-1]
+        
+        Problem.assign(user_to_assign, problem_id)
+        Problem.change_status(problem_id, "assigned")
+      end
+
+      if params['solve_button'] == "Mark as solved"
+        if Problem.is_assigned(problem_id) == false
+          Problem.assign(current_user.username, problem_id)
+        end
+
+        Problem.solve(current_user.username, problem_id)
+        Problem.change_status(problem_id, "solved")
+      end
+
+      redirect_back(fallback_location: root_path)
+    end
+    
+    #setting the search input parameter to display the correct teams
+    if params['search_button'] == "Search" || params['search_form']!= nil
+      @saved_input = params['search_form']['search_input']
+      search_input = params['search_form']['search_input']
+      search_type = params['search_form']['search_type']
+    elsif params['search_input'] != nil
+      @saved_input = params['search_input']
+      search_input = params['search_input']
+      search_type = params['search_type']
+    else
+      @saved_input = ""
+      search_input = ""
+      search_type = "Default - A to Z"
+    end
+
+    
+    #remembering the search input and sorting by the selected search type
+    if search_type == "Team size - Low to High"
+      @selected_type = "Team size - Low to High"
+      search_type = 'count(user_id)'
+    elsif search_type == "Default - A to Z"
+      @selected_type = "Default - A to Z"
+      search_type = 'SUBSTRING(name FROM 1 FOR 4), CAST(SUBSTRING(name FROM 6) AS INTEGER)'
+    elsif search_type == "Team size - High to Low"
+      @selected_type = "Team size - High to Low"
+      search_type = 'count(user_id) DESC'
+    elsif search_type == "Topic - A to Z"
+      @selected_type = "Topic - A to Z"
+      search_type = 'topic'
+    end
+
+    #getting teams for the current search input
+    @groups_for_module = Team.left_outer_joins(:users).where("teams.list_module_id = ? AND
+                                                    (users.givenname LIKE ? OR
+                                                    users.sn LIKE ? OR
+                                                    users.email LIKE ? OR
+                                                    teams.name LIKE ? OR 
+                                                    teams.topic LIKE ?)",
+                                                    params[:module_id],
+                                                    "%" + search_input + "%",
+                                                    "%" + search_input + "%",
+                                                    "%" + search_input + "%",
+                                                    "%" + search_input + "%",
+                                                    "%" + search_input + "%"
+                                                    ).group(:id).order(search_type)
+    
+    #getting the total number of problems
+    @num_of_problems = 0
+    for i in 0..(@groups_for_module.length-1)
+      num_current_problems = Problem.where(team_id: @groups_for_module[i].id).length
+      @num_of_problems = @num_of_problems + num_current_problems
+    end
+
+    if params['search_button'] == "Search" || params['search_form'] != nil
+      mod_id = params['search_form']['form_module_id']
+      redirect_to admin_modules_groups_path(module_id: mod_id, search_input: params['search_form']['search_input'], search_type: params['search_form']['search_type'])
+    end
+
+    #removing a student from a group
+    if params['remove_student_button'] == "remove_student"
+     
+      user_to_remove = UserTeam.where("user_id = ? AND team_id =?",
+                                       params['student_remove_id'],
+                                       params['team_id'])
+
+      if user_to_remove != nil
+        user_to_remove.first.destroy
+      end
+
+      redirect_back(fallback_location: root_path)
+    end
+
+  end
 
 end
