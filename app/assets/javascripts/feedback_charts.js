ac446b7a9c8aa059c80d9268dddbee68423f941f
Anton Minkov <aminkov1@sheffield.ac.uk>
2021-05-08 11:43:40 +0100

    Feedback chart generation + animations

--- app/assets/javascripts/feedback_charts.js
+++ app/assets/javascripts/feedback_charts.js
@@ -2,70 +2,338 @@
 // // Authors: Anton Minkov && Laney Deveson
 // // Date: 07/05/2021
 
-const labels = [
-	'Week 1',
-	'Week 2',
-	'Week 3',
-	'Week 4',
-	'Week 5',
-	'Week 6',
-	'Week 7',
-	'Week 8',
-	'Week 9',
-	'Week 10',
-	'Week 11',
-	'Week 12'
-];
+function generateFeedbackChart() {
 
-const yLabels = {
-	1 : 'Unsatisfactory', 2 : 'Needs Improvement', 3 : 'Meets Expectations', 4 : 'Exceeds Expectations'
-}
-	
-const data = {
-	labels: labels,
-	datasets: [{
-		label: 'Attendance',
-		tooltipText: ['Unsatisfactory', 'Needs Improvement', 'Meets Expectations', 'Exceeds Expectations'],
-		backgroundColor: 'rgb(0, 159, 218)',
-		borderColor: 'rgb(31, 20, 93)',
-		data: [3, 1, 1, 2, 3, 4, 4,3,2,2,4,3],
-	},
-	{
-		label: 'Leadership',
-		hidden: true,
-		backgroundColor: 'rgb(234, 159, 218)',
-		borderColor: 'rgb(90, 180, 93)',
-		data: [2, 3, 1, 1, 4, 2, 4,2,1,3,4,4],
+	const labels = [
+		'Week 1',
+		'Week 2',
+		'Week 3',
+		'Week 4',
+		'Week 5',
+		'Week 6',
+		'Week 7',
+		'Week 8',
+		'Week 9',
+		'Week 10',
+		'Week 11',
+		'Week 12'
+	];
+
+	var yLabels = {
+		1 : 'Unsatisfactory', 2 : ['Needs', 'Improvement'], 3 : ['Meets', 'Expectations'], 4 : ['Exceeds', 'Expectations']
 	}
-	]
-};
 
-const config = {
-	type: 'line',
-	data,
-	options: {
-		plugins: {
-			tooltip: {
-				enabled: true,
-				callbacks: {
-					label: function(tooltipItem, data) {
-						//var title = data.tooltipText[tooltipItem[0].index];
-						return "hello";
+	var line_color = ['#d72631', '#e2d810', '#12a4d9', '#d9138a', '#3b4d61', '#0000B0', '#11bf23', '#d2601a']
+
+	// Average
+	// Attendance and Punctuality
+	// Attitude and Commitment
+	// Quality, accuracy and completeness
+	// Communication
+	// Collaboration
+	// Leadership
+	// Professionalism and ethics
+
+	var feedbackDataArray = [
+		[2,4,4,1,2,3,3,2,1,3,2,4],
+		[1,4,2,3,1,2,3,4,2,1,1,2],
+		[2,3,4,1,2,3,4,1,2,3,3,1],
+		[4,1,2,3,4,1,2,3,4,1,2,4],
+		[2,1,3,1,4,2,1,3,4,4,1,3],
+		[2,3,1,4,2,3,1,4,1,4,3,4],
+		[3,2,1,4,2,3,1,3,4,1,2,2],
+		[3,3,1,1,4,3,2,2,3,4,3,1]
+	]
+
+	const data = {
+		labels: labels,
+		datasets: [
+			{
+				label: 'Average',
+				hidden: false,
+				backgroundColor: line_color[0],
+				borderColor: line_color[0],
+				data: feedbackDataArray[0],
+			},
+			{
+				label: 'Attendance and Punctuality',
+				hidden: true,
+				backgroundColor: line_color[1],
+				borderColor: line_color[1],
+				data: feedbackDataArray[1],
+			},
+			{
+				label: 'Attitude and Commitment',
+				hidden: true,
+				backgroundColor: line_color[2],
+				borderColor: line_color[2],
+				data: feedbackDataArray[2],
+			},
+			{
+				label: 'Quality, accuracy and completeness',
+				hidden: true,
+				backgroundColor: line_color[3],
+				borderColor: line_color[3],
+				data: feedbackDataArray[3],
+			},
+			{
+				label: 'Communication',
+				hidden: true,
+				backgroundColor: line_color[4],
+				borderColor: line_color[4],
+				data: feedbackDataArray[4],
+			},
+			{
+				label: 'Collaboration',
+				hidden: true,
+				backgroundColor: line_color[5],
+				borderColor: line_color[5],
+				data: feedbackDataArray[5],
+			},
+			{
+				label: 'Leadership',
+				hidden: true,
+				backgroundColor: line_color[6],
+				borderColor: line_color[6],
+				data: feedbackDataArray[6],
+			},
+			{
+				label: 'Professionalism and ethics',
+				hidden: true,
+				backgroundColor: line_color[7],
+				borderColor: line_color[7],
+				data: feedbackDataArray[7],
+			},
+		]
+	};
+
+	var chart_element = document.getElementById('feedback_chart').getContext('2d');
+	var myChart = new Chart(chart_element, {
+		type: 'line',
+		data: data,
+		options: {
+			transitions: {
+				show: {
+					animations: {
+						x: {
+							from: 0
+						},
+						y: {
+							from: 0
+						}
+					}
+				},
+				hide: {
+					animations: {
+						x: {
+							to: 0
+						},
+						y: {
+							to: 0
+						}
 					}
 				}
 			},
-		},
-		scales: {
-			y: {
-				max: 5,
-				min: 0,
-				ticks: {
-					stepSize: 0.5,
-					callback: function(value, index, values) {
-						return yLabels[value];
+			responsive: true,
+    	maintainAspectRatio: false,
+			plugins: {
+				tooltip: {
+					enabled: true,
+					callbacks: {
+						labelTextColor:function(tooltipItem, chart){
+								return 'white';
+						},
+						label: function(context) {
+							//This stores the label of the dataset(Leadership)
+							var label = context.dataset.label || '';
+							
+							if (label == 'Average') {
+								if (label) {
+										label += ': ';
+								}
+								//context.parsed.y is the y value for that specific point
+								if (context.parsed.y !== null) {
+									if (context.parsed.y == "1") {
+										label += 'Unsatisfactory'
+									}
+									else if (context.parsed.y == "2") {
+										label += 'Needs Improvement'
+									}
+									else if (context.parsed.y == "3") {
+										label += 'Meets Expectations'
+									}
+									else if (context.parsed.y == "4") {
+										label += 'Exceeds Expectations'
+									}
+										
+								}
+							}
+
+							else if (label == 'Attendance and Punctuality') {
+								if (label) {
+									label += ': ';
+								}
+								//context.parsed.y is the y value for that specific point
+								if (context.parsed.y !== null) {
+									if (context.parsed.y == "1") {
+										label += 'Unsatisfactory'
+									}
+									else if (context.parsed.y == "2") {
+										label += 'Needs Improvement'
+									}
+									else if (context.parsed.y == "3") {
+										label += 'Meets Expectations'
+									}
+									else if (context.parsed.y == "4") {
+										label += 'Exceeds Expectations'
+									}		
+								}
+							}
+
+							else if (label == 'Attitude and Commitment') {
+								if (label) {
+									label += ': ';
+								}
+								//context.parsed.y is the y value for that specific point
+								if (context.parsed.y !== null) {
+									if (context.parsed.y == "1") {
+										label += 'Unsatisfactory'
+									}
+									else if (context.parsed.y == "2") {
+										label += 'Needs Improvement'
+									}
+									else if (context.parsed.y == "3") {
+										label += 'Meets Expectations'
+									}
+									else if (context.parsed.y == "4") {
+										label += 'Exceeds Expectations'
+									}		
+								}
+							}
+
+							else if (label == 'Quality, accuracy and completeness') {
+								if (label) {
+									label += ': ';
+								}
+								//context.parsed.y is the y value for that specific point
+								if (context.parsed.y !== null) {
+									if (context.parsed.y == "1") {
+										label += 'Unsatisfactory'
+									}
+									else if (context.parsed.y == "2") {
+										label += 'Needs Improvement'
+									}
+									else if (context.parsed.y == "3") {
+										label += 'Meets Expectations'
+									}
+									else if (context.parsed.y == "4") {
+										label += 'Exceeds Expectations'
+									}		
+								}
+							}
+
+							else if (label == 'Communication') {
+								if (label) {
+									label += ': ';
+								}
+								//context.parsed.y is the y value for that specific point
+								if (context.parsed.y !== null) {
+									if (context.parsed.y == "1") {
+										label += 'Unsatisfactory'
+									}
+									else if (context.parsed.y == "2") {
+										label += 'Needs Improvement'
+									}
+									else if (context.parsed.y == "3") {
+										label += 'Meets Expectations'
+									}
+									else if (context.parsed.y == "4") {
+										label += 'Exceeds Expectations'
+									}		
+								}
+							}
+
+							else if (label == 'Collaboration') {
+								if (label) {
+									label += ': ';
+								}
+								//context.parsed.y is the y value for that specific point
+								if (context.parsed.y !== null) {
+									if (context.parsed.y == "1") {
+										label += 'Unsatisfactory'
+									}
+									else if (context.parsed.y == "2") {
+										label += 'Needs Improvement'
+									}
+									else if (context.parsed.y == "3") {
+										label += 'Meets Expectations'
+									}
+									else if (context.parsed.y == "4") {
+										label += 'Exceeds Expectations'
+									}		
+								}
+							}
+
+							else if (label == 'Leadership') {
+								if (label) {
+									label += ': ';
+								}
+								//context.parsed.y is the y value for that specific point
+								if (context.parsed.y !== null) {
+									if (context.parsed.y == "1") {
+										label += 'Unsatisfactory'
+									}
+									else if (context.parsed.y == "2") {
+										label += 'Needs Improvement'
+									}
+									else if (context.parsed.y == "3") {
+										label += 'Meets Expectations'
+									}
+									else if (context.parsed.y == "4") {
+										label += 'Exceeds Expectations'
+									}		
+								}
+							}
+
+							else if (label == 'Professionalism and ethics') {
+								if (label) {
+									label += ': ';
+								}
+								//context.parsed.y is the y value for that specific point
+								if (context.parsed.y !== null) {
+									if (context.parsed.y == "1") {
+										label += 'Unsatisfactory'
+									}
+									else if (context.parsed.y == "2") {
+										label += 'Needs Improvement'
+									}
+									else if (context.parsed.y == "3") {
+										label += 'Meets Expectations'
+									}
+									else if (context.parsed.y == "4") {
+										label += 'Exceeds Expectations'
+									}		
+								}
+							}
+
+
+							return label;
+						}
+					}
+				},
+			},
+			scales: {
+				y: {
+					max: 5,
+					min: 0,
+					ticks: {
+						stepSize: 0.5,
+						callback: function(value, index, values) {
+							return yLabels[value];
+						}
 					}
 				}
 			}
 		}
-	}
-};
+	});
+}
