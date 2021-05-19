ccab51525fc4a79cddb65b68da91b8a9eea71faa
Dominik <dlaszczyk1@sheffield.ac.uk>
2021-05-19 18:12:57 +0100

    creating teams form validation

--- app/assets/javascripts/admin_page.js
+++ app/assets/javascripts/admin_page.js
@@ -48,9 +48,36 @@ function create_groups() {
 
   let random_group_size = $("#normal_size").val();
   let random_num_of_groups = $("#normal_amount").val();
+
+  let max_team_size = 100;
+  let max_num_of_teams = 500;
+  let max_topic_length = 100;
   
   if ((random_value == "enabled") && (random_group_size.length > 0 || random_num_of_groups.length > 0)){
-    $("#create_groups_form").trigger('submit.rails');
+    //section for random team creation validation
+
+    if (isNaN(random_group_size)) {
+      myAlertTopEditableError("Team size has to be a number!");
+    }
+    else if (isNaN(random_num_of_groups)) {
+      myAlertTopEditableError("Number of teams has to be a number!");
+    }
+    else if (parseInt(random_group_size) >= max_team_size) {
+      myAlertTopEditableError("Team size has to be less than: " + max_team_size.toString());
+    }
+    else if (parseInt(random_num_of_groups) >= max_num_of_teams) {
+      myAlertTopEditableError("Number of teams has to be less than: " + max_num_of_teams.toString());
+    }
+    else if (parseInt(random_group_size) < 1) {
+      myAlertTopEditableError("Team size has to be greater than 0");
+    }
+    else if (parseInt(random_num_of_groups) < 1) {
+      myAlertTopEditableError("Number of teams has to be greater than 0");
+    }
+    else {
+      $("#create_groups_form").trigger('submit.rails');
+    }
+
   } 
   else if ((random_value == "enabled") && (random_group_size.length == 0 && random_num_of_groups.length == 0)) {
     myAlertTopEditableError("Please select the team size and/or number of teams.");
@@ -66,29 +93,61 @@ function create_groups() {
 
       //check if all topic fields are filled
       if (current_topic_name.length == 0) {
-        myAlertTopEditableError("Please select the name for topic " + (i+1).toString());
+        myAlertTopEditableError("Please select the name for Topic " + (i+1).toString());
         topics_integrity = false;
         break;
       }
       else if ((current_topic_size.length == 0)) {
-        myAlertTopEditableError("Please select the team size for topic " + (i+1).toString());
+        myAlertTopEditableError("Please select the team size for Topic " + (i+1).toString());
         topics_integrity = false;
         break;
       }
       else if ((current_topic_amount.length == 0)) {
-        myAlertTopEditableError("Please select the number of teams for topic " + (i+1).toString());
+        myAlertTopEditableError("Please select the number of teams for Topic " + (i+1).toString());
         topics_integrity = false;
         break;
       }
 
       //check if topic size and topic team amount fields are numbers
       if (isNaN(current_topic_size)) {
-        myAlertTopEditableError("Topic team size of topic " + (i+1).toString() + " is not a number");
+        myAlertTopEditableError("Topic team size of Topic " + (i+1).toString() + " is not a number!");
         topics_integrity = false;
         break;
       }
       else if (isNaN(current_topic_amount)) {
-        myAlertTopEditableError("Number of teams for topic " + (i+1).toString() + " is not a number");
+        myAlertTopEditableError("Number of teams for Topic " + (i+1).toString() + " is not a number!");
+        topics_integrity = false;
+        break;
+      }
+
+      //check if team size and num of teams are less than the threshold
+      if (parseInt(current_topic_size) >= max_team_size) {
+        myAlertTopEditableError("Team size for Topic " + (i+1).toString() + " has to be less than: " + max_team_size.toString());
+        topics_integrity = false;
+        break;
+      }
+      else if (parseInt(current_topic_amount) >= max_num_of_teams) {
+        myAlertTopEditableError("Number of teams for Topic " + (i+1).toString() + " has to be less than: " + max_num_of_teams.toString());
+        topics_integrity = false;
+        break;
+      }
+
+
+      //checking if the current topics length is less than the treshold
+      if (current_topic_name.length >= max_team_size) {
+        myAlertTopEditableError("Length of name for Topic " + (i+1).toString() + " has to be less than: " + max_topic_length.toString());
+        topics_integrity = false;
+        break;
+      }
+
+      //checking if team size and num of teams is a positive number
+      if (parseInt(current_topic_size) < 1) {
+        myAlertTopEditableError("Team size for Topic " + (i+1).toString() + " has to be greater than 0");
+        topics_integrity = false;
+        break;
+      }
+      else if (parseInt(current_topic_amount) < 1) {
+        myAlertTopEditableError("Number of teams for Topic " + (i+1).toString() + " has to be greater than 0");
         topics_integrity = false;
         break;
       }
